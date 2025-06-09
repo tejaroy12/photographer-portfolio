5b7c18b7fa02aa243b988b0d55bc4b3335fb235f
tejaroy12 <112643817+tejaroy12@users.noreply.github.com>
2025-06-09 12:21:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 const galleryRoutes = require('./routes/galleryRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
@@ -15,7 +16,7 @@ app.use('/api/locations', galleryRoutes);
 app.use('/api/bookings', bookingRoutes);
 
 // Serve uploaded images
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
